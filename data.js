037e0687761fcf0ca850d5c343e3d696d8e6ff8b
drumismylife <drumismylife@gmail.com>
2026-09-23 00:58:23 +0900

    강의노트 업데이트: 교회사2 week03 (제목 구체화)

--- data.js
+++ data.js
@@ -571,7 +571,7 @@ const SITE_DATA = {
           weeks: [
             { week: 1, title: "", date: "", files: [] },
             { week: 2, title: "", date: "", files: [] },
-            { week: 3, title: "제6부 종교개혁과 개신교의 시대 (1517-1648) - 3주차", date: "2026.09.15", files: [
+            { week: 3, title: "3주차 마틴 루터와 제1세대 종교개혁", date: "2026.09.15", files: [
             { type: "notes", label: "강의노트", href: "output/2026-2/hist2/week03.html" }
           ] },
             { week: 4, title: "", date: "", files: [] },
